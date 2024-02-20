1dd9023b4734d8489122fcce86e31d52e156b9e7
mamadou <mimadou97@yahoo.com>
2024-02-20 16:09:00 +0100

    configuration du back

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use((req , res , next)=>{
+    res.json({message : " phase "})
+    next();
+});
+
+app.use((req , res) => {
+    console.log("le restaurant epicurian")
+})
+
+module.exports = app;
